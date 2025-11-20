f59f73eb729332e508902c521346772d4dc5e8be
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-11-20 22:31:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ class Vars:
   
   LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002456565415")
   UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "-1001987570479")
-  DB_URL = os.environ.get("DB_URL", "")
+  DB_URL = os.environ.get("DB_URL", "mongodb+srv://Kafka:Au3OoWzCDYJKeuHU@cluster0.lz2m8iy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   
   PORT = int(os.environ.get("PORT", "5000"))
   OWNER = int(os.environ.get("OWNER","7654385403"))
