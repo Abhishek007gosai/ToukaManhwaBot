ed46e5b6d808a62c441291550b8840fef0b0977e
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-11-22 01:37:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,8 +33,8 @@ class Vars:
   DB_NAME = "cluster0"
   PING = time()
   
-  SHORTENER = True
-  SHORTENER_API = "64d631b036df348caab852591a09288cbf5b6809?shortxlinks.com={}" # put {} for url, ex: shornter.api?url={}
+  SHORTENER = False
+  SHORTENER_API = "" # put {} for url, ex: shornter.api?url={}
   DURATION = 20 # hrs
   
   FORCE_SUB_TEXT = os.environ.get("FORCE_SUB_TEXT", """<b><blockquote>Hᴇʟʟᴏ!! ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ <a href=https://t.me/MangaNexus>ᴍᴀɴɢᴀ ɴᴇxᴜs</blockquote> </a> Yᴏᴜ ɴᴇᴇᴅ ᴛᴏ Jᴏɪɴ ɪɴ ᴍʏ Cʜᴀɴɴᴇʟ/Gʀᴏᴜᴘ ғɪʀsᴛ, Pʟᴇᴀsᴇ sᴜʙsᴄʀɪʙᴇ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴛʜʀᴏᴜɢʜ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ʙᴇʟᴏᴡ ᴀɴᴅ sᴛᴀʀᴛ ʙᴏᴛ ᴀɢᴀɪɴ</b>""")
