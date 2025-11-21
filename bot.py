ae2440103783ded6cb7f6df8a74edab4d77b167a
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-11-22 00:09:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ class Vars:
   API_ID = int(os.environ.get("API_ID", "23537462"))
   API_HASH = os.environ.get("API_HASH", "c9599a5aa61ee8ca4f5e778d20c61f24")
   
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7845096754:AAFoHy9LeNM9NNhXlSWFOlEk3rRjppBpo-8")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7689010904:AAFJ48GX1zDr7mxPQxReDbgSSvp-e0Hcw_Y")
   plugins = dict(root="TG")
   
   LOG_CHANNEL = -1002456565415
