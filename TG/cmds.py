5d9d3ed060f8d8fa89d8083b66e7b6cb6764bf94
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-11-23 02:20:08 +0530

    Update cmds.py

--- TG/cmds.py
+++ TG/cmds.py
@@ -138,48 +138,46 @@ async def start(client, message):
       sts = await message.reply("<i>ㅤProcessing.....</i>")
       return await verify_token(sts, user_id, token)
 
-photo = random.choice(Vars.PICS)
-ping = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - Vars.PING))
+    photo = random.choice(Vars.PICS)
+    ping = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - Vars.PING))
 
-await message.reply_photo(
-    photo,
-    caption=(
-        "<b><blockquote>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴜᴘᴘᴏʀᴛ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ ʏᴏᴜ ᴄᴀɴ ᴅᴏ sᴏ ʙʏ sᴜʙsᴄʀɪʙɪɴɢ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ</blockquote></b>\n"
-        "<b>ʜᴏᴡ ᴛᴏ ᴜsᴇ? ɪᴜsᴛ ᴛʏᴘᴇ ᴛʜᴇ ɴᴀᴍᴇ ᴏғ sᴏᴍᴇ ᴍᴀɴɢᴀ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴋᴇᴇᴘ ᴜᴘ ᴛᴏ ᴅᴀᴛᴇ.</b>\n"
-        "<b>ғᴏʀ ᴇxᴀᴍᴘʟᴇ:</b>\n"
-        "<i><code>One Piece</code></i>\n"
-        "<b>ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴄʟɪᴄᴋ ʜᴇʀᴇ /help</b>\n"
-        "<b><blockquote>ᴀᴅᴍɪɴ sᴜᴘᴘᴏʀᴛ @EternalsHelplineBot</blockquote></b>"
-    ),
-    reply_markup=InlineKeyboardMarkup(
-        [
+    await message.reply_photo(
+        photo,
+        caption=(
+            "<b><blockquote>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴜᴘᴘᴏʀᴛ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ ʏᴏᴜ ᴄᴀɴ ᴅᴏ sᴏ ʙʏ sᴜʙsᴄʀɪʙɪɴɢ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ</blockquote></b>\n"
+            "<b>ʜᴏᴡ ᴛᴏ ᴜsᴇ? ɪᴜsᴛ ᴛʏᴘᴇ ᴛʜᴇ ɴᴀᴍᴇ ᴏғ sᴏᴍᴇ ᴍᴀɴɢᴀ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴋᴇᴇᴘ ᴜᴘ ᴛᴏ ᴅᴀᴛᴇ.</b>\n"
+            "<b>ғᴏʀ ᴇxᴀᴍᴘʟᴇ:</b>\n"
+            "<i><code>One Piece</code></i>\n"
+            "<b>ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴄʟɪᴄᴋ ʜᴇʀᴇ /help</b>\n"
+            "<b><blockquote>ᴀᴅᴍɪɴ sᴜᴘᴘᴏʀᴛ @EternalsHelplineBot</blockquote></b>"
+        ),
+        reply_markup=InlineKeyboardMarkup(
             [
-                InlineKeyboardButton(
-                    "⌜ᴄᴏᴍᴍᴀɴᴅs ᴀɴᴅ sᴇᴛᴛɪɴɢs⌟",
-                    callback_data="mus"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "sᴜᴘᴘᴏʀᴛ",
-                    url="https://t.me/EternalsHelplineBot"
-                ),
-                InlineKeyboardButton(
-                    "ᴜᴘᴅᴀᴛᴇ",
-                    url="https://t.me/MangaNexus"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    "⌜ᴄʟᴏsᴇ⌟",
-                    callback_data="close"
-                )
+                [
+                    InlineKeyboardButton(
+                        "⌜ᴄᴏᴍᴍᴀɴᴅs ᴀɴᴅ sᴇᴛᴛɪɴɢs⌟",
+                        callback_data="mus"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "sᴜᴘᴘᴏʀᴛ",
+                        url="https://t.me/EternalsHelplineBot"
+                    ),
+                    InlineKeyboardButton(
+                        "ᴜᴘᴅᴀᴛᴇ",
+                        url="https://t.me/MangaNexus"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "⌜ᴄʟᴏsᴇ⌟",
+                        callback_data="close"
+                    )
+                ]
             ]
-        ]
+        )
     )
-)
-
-
 
 
 
